76f1ea275caf2c1f7bbf12365226a3dfc93a7c2d
XDyao <56805700+XDyao@users.noreply.github.com>
2020-04-23 17:06:13 -0300

    Update grafo.c

---
+++ src/grafo.c
@@ -0,0 +1,15 @@
+#include "grafo.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void cria_grafo(Grafo* g, int tam)
+
+int busca_aresta(Grafo* g, int tam)
+
+void adiciona_aresta(Grafo* g, int x, int y)
+
+void remove_aresta(Grafo* g, int x, int y)
+
+void imprime_grafo(Grafo* g, int tam)
+
+void apaga_grafo(Grafo* g, int tam)
