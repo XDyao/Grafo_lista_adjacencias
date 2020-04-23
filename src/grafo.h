7403f0c5766ea1e230b532aec3002370564f68b1
matheustomieiro <matheustomieiro@usp.br>
2020-04-23 18:28:54 -0300

    Finalizado!

--- src/grafo.h
+++ src/grafo.h
@@ -1,8 +1,8 @@
 typedef struct grafo Grafo;
 
-void cria_grafo(Grafo* g, int tam);
-int busca_aresta(Grafo* g, int tam);
+Grafo *cria_grafo(int tam);
+int busca_aresta(Grafo* g, int x, int y);
 void adiciona_aresta(Grafo* g, int x, int y);
 void remove_aresta(Grafo* g, int x, int y);
-void imprime_grafo(Grafo* g, int tam);
-void apaga_grafo(Grafo* g, int tam);
+void imprime_grafo(Grafo* g);
+void apaga_grafo(Grafo* g);
